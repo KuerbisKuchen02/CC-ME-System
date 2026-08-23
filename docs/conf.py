cc405d83edc0cc278ef6aada41f4035df5a1c912
Josia Menger <josia.menger@gmx.de>
2026-08-23 18:35:28 +0200

    docs: add sphinx getting started project

---
+++ docs/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "CC Tools"
+copyright = "2026, KuerbisKuchen"
+author = "KuerbisKuchen"
+release = "0.1-alpha"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx_lua_ls",
+    "sphinx_markdown_builder",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# Sphinx LuaLs
+lua_ls_backend = "luals"
+lua_ls_project_root = "../src"
+lua_ls_project_directories = [
+    "client",
+    "server",
+    "common",
+]
+lua_ls_verbose = True
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
